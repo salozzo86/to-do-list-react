1ba2d09048524a872096f3d470dcc7f90c7a9373
salozzo86 <antonio.malafronte86@gmail.com>
2023-10-22 09:57:34 +0200

    Refactored to avoid repetitions due to styling

--- src/components/todos/ToDoItem.jsx
+++ src/components/todos/ToDoItem.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const ToDoItem = (props) => {
     const [isCompleted, setIsCompleted] = useState(false);
@@ -9,39 +9,33 @@ const ToDoItem = (props) => {
         setIsCompleted((prevState) => !prevState);
     };
 
+    const itemStyle = useMemo(() => {
+        const baseStyles =
+            'my-4 flex w-80 items-center rounded px-2 py-4 text-center shadow-md';
+        if (isCompleted) {
+            return `${baseStyles} bg-[#E8E0F6] text-slate-400 dark:bg-[#A6BBA0] dark:text-slate-500`;
+        } else {
+            return `${baseStyles} bg-[#BEA7E5] dark:bg-[#5B7553] dark:text-white`;
+        }
+    }, [isCompleted]);
+
     return (
         <>
-            {isCompleted ? (
-                <section className="my-4 flex w-80 items-center rounded bg-[#E8E0F6] px-2 py-4 text-center text-slate-400 shadow-md dark:bg-[#A6BBA0] dark:text-slate-500">
-                    <li className="flex-grow">
-                        <div className="text-base font-bold line-through">
-                            {props.summary}
-                        </div>
-                        <div className="text-sm line-through">
-                            {props.notes}
-                        </div>
-                    </li>
-                    <FontAwesomeIcon
-                        icon={faSquareCheck}
-                        className="px-5"
-                        onClick={isCompletedHandler}
-                    />
-                </section>
-            ) : (
-                <section className="my-4 flex w-80 items-center rounded bg-[#BEA7E5] px-2 py-4 text-center shadow-md dark:bg-[#5B7553] dark:text-white">
-                    <li className="flex-grow">
-                        <div className="text-base font-bold">
-                            {props.summary}
-                        </div>
-                        <div className="text-sm">{props.notes}</div>
-                    </li>
-                    <FontAwesomeIcon
-                        icon={faSquareCheck}
-                        className="px-5"
-                        onClick={isCompletedHandler}
-                    />
-                </section>
-            )}
+            <section className={itemStyle}>
+                <li
+                    className={
+                        isCompleted ? 'flex-grow line-through' : 'flex-grow'
+                    }
+                >
+                    <div className="text-base font-bold">{props.summary}</div>
+                    <div className="text-sm">{props.notes}</div>
+                </li>
+                <FontAwesomeIcon
+                    icon={faSquareCheck}
+                    className="px-5"
+                    onClick={isCompletedHandler}
+                />
+            </section>
         </>
     );
 };
